81c51861f86d922cdf266216cc5360c052fd67b3
Michael <michaelandgobaw@gmail.com>
2024-03-31 16:53:22 +0900

    add document

--- js/main.js
+++ js/main.js
@@ -1,23 +1,19 @@
-let menu = document.querySelector('.fa-bars');
-let navbar = document.querySelector('.navbar');
+document.addEventListener('DOMContentLoaded', function () {
+  const boxes = document.querySelectorAll('.box');
+  const fullpage = document.getElementById('fullpage');
 
-menu.addEventListener('click',function(){
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('nav-toggle');
-});
+  boxes.forEach(function (box) {
+      box.addEventListener('click', function () {
+          const imgSrc = this.querySelector('img').src;
+          fullpage.innerHTML = '<img src="' + imgSrc + '" alt="popup-image">';
+          fullpage.style.display = 'block';
+      });
+  });
 
-window.addEventListener('scroll', () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('nav-toggle');
-});
-
-function getPics() {} //just for this demo
-const imgs = document.querySelectorAll('.gambar img');
-const fullPage = document.querySelector('#fullpage');
-
-imgs.forEach(img => {
-  img.addEventListener('click', function() {
-    fullPage.style.backgroundImage = 'url(' + img.src + ')';
-    fullPage.style.display = 'block';
+  // Close the full page popup when clicking outside the image
+  fullpage.addEventListener('click', function (event) {
+      if (event.target === this) {
+          this.style.display = 'none';
+      }
   });
 });
